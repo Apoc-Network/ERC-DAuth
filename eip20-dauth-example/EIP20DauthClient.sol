5fced067aaefd48bd5626189893181328f4f8aba
wangbicong <wangbicong@hotmail.com>
2018-07-10 15:04:14 +0800

    feat: add EIP20Dauth Client Demo Contract

---
+++ eip20-dauth-example/EIP20DauthClient.sol
@@ -0,0 +1,48 @@
+/*
+Implements EIP20Dauth Client to invoke EIP20Dauth contract.
+*/
+pragma solidity ^0.4.24;
+
+import "./EIP20Dauth.sol";
+
+
+contract EIP20DauthClient {
+    
+    address private owner;
+    EIP20Dauth private eip20Dauth;
+
+    constructor() public {
+        owner = msg.sender;     // Default Owner: Contract Deployer
+    }
+    
+    /* Modifiers:
+        Authorization Call Handler MUST be invoked only by owner.
+    */
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+    function setOwner (address _owner) onlyOwner() public {
+        owner = _owner;
+    }
+        
+    function setEip20Dauth(address _dauthAddress) onlyOwner() public {
+        eip20Dauth = EIP20Dauth(_dauthAddress);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) onlyOwner() public returns (bool success) {
+        return eip20Dauth.transfer(_to, _value, _from);
+    }
+    
+    function approve(address _authorizer, address _spender, uint256 _value) onlyOwner() public returns (bool success) {
+        return eip20Dauth.approve(_spender, _value, _authorizer);
+    }
+    
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return eip20Dauth.balanceOf(_owner);
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return eip20Dauth.allowance(_owner, _spender);
+    }
+}
